24855cdb6dd145e47d450895f0b888c15bcc3c54
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-08-27 07:54:53 -0400

    First commit, added if conditions and Logical operators

---
+++ conditionals.js
@@ -0,0 +1,28 @@
+const raining = true;
+// const cold = false;
+const temperature = 12;
+
+if (raining) {
+  console.log("Don't forget your umbrella");
+}
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+if (temperature < -40 && temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea...");
+}
+
+console.log("Now you're ready to go out outside");
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
