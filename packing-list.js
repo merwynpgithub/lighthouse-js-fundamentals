2fe79a61c1848138b4f4673635890c1f865f141e
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-09-01 06:57:17 -0400

    Iterating over an array using a for and while loop

---
+++ packing-list.js
@@ -0,0 +1,15 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:")
+
+// Using a for loop to iterate over the array
+// for (let i = 0; i < packingList.length; i++) {
+//   console.log(packingList[i]);
+// }
+
+// Using a while loop to iterate over the array
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
